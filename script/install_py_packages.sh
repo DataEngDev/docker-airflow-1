a1ddaded0458099affda16cab817072dba2069ee
yennan.liu <yennan.liu@rtwpc30008.intra.rakuten.co.jp>
2021-01-14 19:03:52 +0800

    add /script

---
+++ script/install_py_packages.sh
@@ -0,0 +1,8 @@
+pip install --upgrade pip
+pip install hdfs certifi cffi chardet cryptography docopt future idna Jinja2 MarkupSafe pycparser python-dateutil pywebhdfs requests six thrift urllib3
+
+# PyHive
+# pykerberos
+# requests-kerberos
+# sasl
+# thrift-sasl
